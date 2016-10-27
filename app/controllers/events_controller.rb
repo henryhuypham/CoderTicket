09131de8bc6affdb2aa8d383fbba1603ae139f48
Huy Pham <huyphamthanh8290@gmail.com>
2016-10-28 02:28:21 +0700

    Add Search events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,19 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.where('starts_at >= ?', Date.today)
+    if (search_term = search_param)
+      @events = Event.where('lower(name) LIKE lower(?) AND starts_at >= ?', "%#{search_term.split.join('%')}%", Date.today)
+    else
+      @events = Event.where('starts_at >= ?', Date.today)
+    end
   end
 
   def show
     @event = Event.find(params[:id])
+  end
+
+  private
+
+  def search_param
+    params[:search][:event_name] if params[:search].present?
   end
 end
